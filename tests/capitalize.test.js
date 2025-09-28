7f32292b956fd5bbc4fd50e53951006ad26741ea
AnastasyKip <1746237@gmail.com>
2025-09-28 18:58:34 +0300

    Added assert lib

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,12 +1,8 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно! (hello)');
-}
-
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно! (empty string)');
-}
+// Тесты
+assert.strictEqual(capitalize('hello'), 'Hello', 'Функция работает неверно! (hello)');
+assert.strictEqual(capitalize(''), '', 'Функция работает неверно! (empty string)');
 
 console.log('Все тесты пройдены!');
-
